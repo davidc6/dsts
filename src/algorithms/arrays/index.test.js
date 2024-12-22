802411b843ec539b0f4b93cad9b28aa3fcd1e298
David Chubabriya <david.chubabriya@gmail.com>
2024-12-22 18:34:45 +0000

    Clean up arrays algorithms

--- src/algorithms/arrays/index.test.js
+++ src/algorithms/arrays/index.test.js
@@ -5,7 +5,6 @@ import { longest } from './'
 describe('Array algorithms', () => {
     it('Longest consecutive sequence', () => {
         const arr = [0, 3, 2, 5, 4, 6, 1, 1];
-
         const val = longest(arr);
 
         expect(val).to.equal(7);
