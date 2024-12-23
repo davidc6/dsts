cc66308e566346b9a9855199d91abfc21e28ccb7
David Chubabriya <david.chubabriya@gmail.com>
2024-12-23 14:29:37 +0000

    Add flatten deeply nested array algo

--- src/algorithms/arrays/index.test.js
+++ src/algorithms/arrays/index.test.js
@@ -1,12 +1,19 @@
 import { expect } from 'chai'
 import { describe } from 'mocha'
-import { longest } from './'
+import { longestConsecutiveNumber, flattenDeeplyNestedArray } from './'
 
 describe('Array algorithms', () => {
     it('Longest consecutive sequence', () => {
         const arr = [0, 3, 2, 5, 4, 6, 1, 1];
-        const val = longest(arr);
+        const val = longestConsecutiveNumber(arr);
 
         expect(val).to.equal(7);
-    })
+    });
+
+    it('Flatten deeply nested  array', () => {
+        const arr = [[1, 2, 3], [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+        const val = flattenDeeplyNestedArray(arr, 2);
+
+        expect(val).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+    });
 });
--- src/algorithms/arrays/index.ts
+++ src/algorithms/arrays/index.ts
@@ -1,6 +1,6 @@
 // Longest consecutive number
 
-export const longest = (a: number[]): number => {
+export const longestConsecutiveNumber = (a: number[]): number => {
     let longest = 0;
 
     const numbers = new Set(a);
@@ -17,4 +17,32 @@ export const longest = (a: number[]): number => {
     }
 
     return longest;
+}
+
+export const flattenDeeplyNestedArray = (arr: any[], depth: number) => {
+    if (depth == 0) {
+        return arr;
+    }
+
+    let n: any[] = [];
+
+    recurringNested(arr, depth, n);
+
+    return n;
+}
+
+const recurringNested = (arr: any[], depth: number, n: any[]) => {
+    for (const val of arr) {
+        if (val instanceof Array) {
+            recurringNested(val, depth - 1, n);
+        } else {
+            n.push(val);
+        }
+    }
+
+    if (depth === 0) {
+        return n;
+    }
+
+    return n
 }
