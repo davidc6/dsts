b24d5af93afd210f3bb3ec736c85dd748c0d93b5
David Chubabriya <david.chubabriya@gmail.com>
2025-01-01 23:34:15 +0000

    Clean up tree tests

--- src/algorithms/trees/index.test.js
+++ src/algorithms/trees/index.test.js
@@ -3,8 +3,7 @@ import { describe } from 'mocha'
 import { TreeNode } from './'
 
 describe('Tree algorithms', () => {
-    it('Invert binary tree', () => {
-        // const arr = [1, 2, 3, 4, 5, 6, 7];
+    it('Invert a binary tree', () => {
         const t = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         t.left.left = new TreeNode(4);
         t.left.right = new TreeNode(5);
@@ -12,13 +11,11 @@ describe('Tree algorithms', () => {
         t.right.right = new TreeNode(7);
 
         expect(t.toArray()).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
-
         t.invert();
-
         expect(t.toArray()).to.deep.equal([1, 3, 2, 7, 6, 5, 4]);
     });
 
-    it('Max depth of tree', () => {
+    it('Calculate max depth of a tree', () => {
         // level 1
         const t = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         // level 2
@@ -32,7 +29,7 @@ describe('Tree algorithms', () => {
         expect(t.maxDepth()).to.equal(3);
     });
 
-    it('Tree diameter', () => {
+    it('Calculate diameter of a tree', () => {
         const t = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         t.left.left = new TreeNode(4);
         t.left.right = new TreeNode(5);
