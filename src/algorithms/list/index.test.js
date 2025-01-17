2b23c6fe8f0cc7cfbb4e9521adf494274cd650bb
David Chubabriya <david.chubabriya@gmail.com>
2025-01-17 23:20:45 +0000

    Add skip list add initial test

---
+++ src/algorithms/list/index.test.js
@@ -0,0 +1,19 @@
+import chai from 'chai'
+import { describe } from 'mocha'
+import deepEqualInAnyOrder from 'deep-equal-in-any-order';
+import { SkipListNode, SkipList } from './'
+
+chai.use(deepEqualInAnyOrder);
+
+const { expect } = chai;
+
+describe.only('List algorithms', () => {
+    it('Skip list size works', () => {
+        let skipList = new SkipList(5);
+        skipList.insert(1);
+        skipList.insert(3);
+        skipList.insert(10);
+
+        expect(skipList.size()).to.equal(3);
+    })
+})
