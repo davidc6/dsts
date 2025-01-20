8e99ac0eb7cacc7fc51b79d577fa715666e83825
David Chubabriya <david.chubabriya@gmail.com>
2025-01-20 21:18:40 +0000

    In progress of skip list implementation

--- src/algorithms/list/index.test.js
+++ src/algorithms/list/index.test.js
@@ -9,6 +9,34 @@ describe('List algorithms', () => {
         skipList.insert(3);
         skipList.insert(10);
 
-        expect(skipList.size()).to.equal(3);
+        // level 5 value null
+        // level 2 value 1
+        // level 2 value 3
+        // level 1 value 10
+        // console.log(skipList.head.next[0]);
+
+        // console.log(
+
+        // let v = skipList.head;
+
+        // console.log(skipList.head.next[0])
+
+        // while (v) {
+        //     let i = 0;
+        //     let cell = v.next[i];
+
+        //     while (cell) {
+        //         console.log('level: ', cell.level, ' | value: ', cell.value)
+        //         i += 1;
+        //     }
+
+        //     v = v.next[0]
+        // }
+
+        console.log(skipList.print());
+
+
+
+        // expect(skipList.size()).to.equal(3);
     })
 })
--- src/algorithms/list/index.ts
+++ src/algorithms/list/index.ts
@@ -27,6 +27,49 @@ export class SkipList {
         this.head = new SkipListNode(this.maximumLevel);
     }
 
+    // TODO/FIX
+    _print(nodes: SkipListNode[] = this.head.next) {
+        if (!nodes || !nodes.length) {
+            console.log('=======================');
+            return;
+        }
+
+        for (const node of nodes) {
+            // if (node) {
+            this._print(node.next)
+            // }
+
+            console.log('PRINT: ', node)
+        }
+    }
+
+    print() {
+        // this._print(this.head.next)
+
+        // console.log(this.head);
+
+        let cursor = this.head;
+
+        let i = 0;
+
+        while (cursor) {
+            let first = cursor;
+            first = cursor.next[i]
+            // i += 1;
+
+            while (first.next) {
+                console.log(first.value)
+                i += 1;
+            }
+
+            cursor
+        }
+    }
+
+    /**
+     * Determine the level where the value will be inserted
+     * @returns number
+     */
     private generateLevel() {
         let level = 1;
         while (Math.random() < 0.5 && level < this.maximumLevel) {
@@ -45,8 +88,10 @@ export class SkipList {
         let head: SkipListNode = this.head;
 
         // start going down from the top level / layer
+        // i.e if MAX level is 5 then 4, 3, 2, 1, 0
         // while above the 0th layer
         for (let i = this.head.level - 1; i >= 0; i--) {
+            // while next 
             while (head.next[i] && head.next[i].value - value < 0) {
                 // head is now next item in layer array
                 head = head.next[i];
@@ -55,25 +100,29 @@ export class SkipList {
             update[i] = head;
         }
 
-        head = head.next[0];
+        head = head.next[0]; // scoped to this function head is not set to first element
 
-        if (head && head.value && (head.value - value == 0)) {
-            // update
+        // If exists and set to a value and subtracting it's value from value we are trying to insert is 0
+        if (head && head.value && (head.value - value == 0)) { // update
             head.value = value;
             return false;
-        } else {
-            // insert
+        } else { // insert
+            // generate "insertion" level
             const level = this.generateLevel();
 
+            // generated level larger than current level
             if (level > this.currentLevel) {
+                // go up all levels and insert the head
                 for (let i = this.currentLevel; i < level; i++) {
                     update[i] = this.head
                 }
+
                 this.currentLevel = level;
             }
 
             head = new SkipListNode(level, value);
 
+            // append next items
             for (let i = 0; i < level; i++) {
                 head.next[i] = update[i].next[i];
                 update[i].next[i] = head;
